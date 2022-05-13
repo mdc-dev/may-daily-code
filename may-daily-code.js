5f3c067f443c4f062537833d66f861c2dd880012
Matthew Courtney <mcourtdev@gmail.com>
2022-05-13 11:03:26 -0400

    problem solution for day 7

--- may-daily-code.js
+++ may-daily-code.js
@@ -215,3 +215,44 @@ missing([true, true, false, false, true], [false, true, false, true])
 missing(["Jane", "is", "pretty", "ugly"], ["Jane", "is", "pretty"]) 
 // ➞ "ugly"
 
+// May 7 Weekly Salary
+// Write a function that takes a list of hours and returns the total weekly salary.
+// The input list hours is listed sequentially, ordered from Monday to Sunday.
+// A worker earns $10 an hour for the first 8 hours.
+// For every overtime hour, he earns $15.
+// On weekends, the employer pays double the usual rate, regardless how many hours 
+// were worked previously that week. For instance, 10 hours worked on a weekday would pay 80+30 = $110, 
+// but on a weekend it would pay 160+60 = $220.
+
+function weeklySalary(hours) {
+    let totalPay = 0;
+    for (let i = 0; i < 5; i++) {
+        const day = hours[i];
+        if(day > 8) {
+            totalPay += 80;
+            totalPay += (day - 8) * 15
+        } else {
+            totalPay += day * 10
+        }
+    }
+    for (let i = 5; i < hours.length; i++) {
+        const day = hours[i];
+        if(day > 8) {
+            totalPay += 160
+            totalPay += (day - 8) * 30
+        } else {
+           totalPay += day * 20 
+        }
+    }
+
+    return totalPay
+}
+
+weeklySalary([8, 8, 8, 8, 8, 0, 0]) 
+// ➞ 400
+
+weeklySalary([10, 10, 10, 0, 8, 0, 0]) 
+// ➞ 410
+
+weeklySalary([0, 0, 0, 0, 0, 12, 0]) 
+// ➞ 280
