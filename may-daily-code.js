c4da453bff21cf6e0997c749f0c0f0f68b5ef7d5
Matthew Courtney <mcourtdev@gmail.com>
2022-05-13 00:55:38 -0400

    problem solution for day 6

--- may-daily-code.js
+++ may-daily-code.js
@@ -197,3 +197,21 @@ xPronounce("The x ray is excellent")
 xPronounce("OMG x box unboxing video x D") 
 // ➞ "OMG ecks bocks unbocksing video ecks D"
 
+// May 6 Who Left The Array
+// You are given two arrays. The elements in arr1 threw a party and started to mix around. However, 
+// one of the elements got lost! Your task is to return the element which was lost.
+
+function missing(array1, array2) {
+    let missingItem = array1.find((item, i) => item !== array2[i])
+    return missingItem;
+}
+
+missing([1, 2, 3, 4, 5, 6, 7, 8], [1, 3, 4, 5, 6, 7, 8]) 
+// ➞ 2
+
+missing([true, true, false, false, true], [false, true, false, true]) 
+// ➞ true
+
+missing(["Jane", "is", "pretty", "ugly"], ["Jane", "is", "pretty"]) 
+// ➞ "ugly"
+
