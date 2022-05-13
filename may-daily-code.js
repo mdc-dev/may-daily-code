66cb42a04e060477b726a3e179a649606ac00f78
Matthew Courtney <mcourtdev@gmail.com>
2022-05-13 11:22:59 -0400

    problem solution or day 8

--- may-daily-code.js
+++ may-daily-code.js
@@ -256,3 +256,50 @@ weeklySalary([10, 10, 10, 0, 8, 0, 0])
 
 weeklySalary([0, 0, 0, 0, 0, 12, 0]) 
 // ➞ 280
+
+
+// May 8 Burglary Series (22): Sign All
+// You finally receive the entire stolen list document from the police. You need to sign at the end of the 
+// document but also each sub-list corresponding to each room in your house where the items were stolen.
+// You are given two arguments, one object with nested objects and a string that corresponds to your name. 
+// The object already contains several signature properties, one on the root, the others on each nested 
+// object. Return an object with all containing signature values set to your name.
+
+function signAll(obj, name) {
+    const keys = Object.keys(obj);
+    keys.forEach(key => {
+        if(key === 'signature') {
+            obj[key] = name;
+        } else {
+            obj[key].signature = name;
+        }
+    })
+    return obj
+}
+
+const obj = {
+    kitchen: {
+      painting: 100,
+      piano: 1000,
+      signature: "",
+    },
+    bathroom: {
+      stereo: 220,
+      signature: "",
+    },
+    signature: "",
+  };
+
+signAll(obj, "Rocky") 
+// ➞ {
+//     kitchen: {
+//       painting: 100,
+//       piano: 1000,
+//       signature: "Rocky",  // changed
+//     },
+//     bathroom: {
+//       stereo: 220,
+//       signature: "Rocky",  // changed
+//     },
+//     signature: "Rocky",  // changed
+// }
