796dde06f2a4c024550d8d0d2859d015328af6b3
Matthew Courtney <mcourtdev@gmail.com>
2022-05-13 16:38:30 -0400

    problem solution for day 13

--- may-daily-code.js
+++ may-daily-code.js
@@ -420,3 +420,46 @@ uncensor("abcd", "")
 
 uncensor("*PP*RC*S*", "UEAE") 
 // ➞ "UPPERCASE"
+
+// May 13 The Frugal Gentleman
+// Atticus has been invited to a dinner party, and he decides to purchase a bottle of wine. 
+// However, he has little knowledge of how to choose a good bottle. Being a very frugal gentleman 
+// (yet disliking looking like a cheapskate), he decides to use a very simple rule. In any selection 
+// of two or more wines, he will always buy the second-cheapest.
+// Given an array of wine objects, write a function that returns the name of the wine he will buy for the 
+// party. If given an empty array, return null. If given an array of only one, Atticus will buy that wine.
+
+function chosenWine(wineList) {
+    let prices = wineList.map(wine => {
+        return wine.price;
+    });
+
+    if(wineList.length === 0) {
+        return null
+    } else if (prices.length === 1) {
+        let result = wineList.find(wine => {
+            return wine.price === prices[0]
+        });
+        return result.name
+    } else {
+        let chosenPrice = prices.sort((a, b) => a - b)[1]
+        let result = wineList.find(wine => {
+            return wine.price === chosenPrice
+        });
+        return result.name
+    }
+    
+}
+
+chosenWine([
+  { name: "Wine A", price: 8.99 },
+  { name: "Wine 32", price: 13.99 },
+  { name: "Wine 9", price: 10.99 }
+]) 
+// ➞ "Wine 9"
+
+chosenWine([{ name: "Wine A", price: 8.99 }]) 
+// ➞ "Wine A"
+
+chosenWine([]) 
+// ➞ null
