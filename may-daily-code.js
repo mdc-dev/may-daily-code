39b2f1d83dc4b0650bbd36049c729d5f2e10a80b
Matthew Courtney <mcourtdev@gmail.com>
2022-05-12 23:52:21 -0400

    problem solution for day 3

--- may-daily-code.js
+++ may-daily-code.js
@@ -42,17 +42,68 @@ function oddishOrEvenish(number) {
 }
 
 
-oddishOrEvenish(43) 
+oddishOrEvenish(43); 
 // ➞ "Oddish"
 // 4 + 3 = 7
 // 7 % 2 = 1
 
-oddishOrEvenish(373) 
+oddishOrEvenish(373);
 // ➞ "Oddish"
 // 3 + 7 + 3 = 13
 // 13 % 2 = 1
 
-oddishOrEvenish(4433) 
+oddishOrEvenish(4433);
 // ➞ "Evenish"
 // 4 + 4 + 3 + 3 = 14
 // 14 % 2 = 0
+
+
+// May 3 Calculate the Total Price of Groceries
+// Create a function that takes an array of objects (groceries) which calculates the total price and 
+// returns it as a number. A grocery object has a product, a quantity and a price
+
+function getTotalPrice(array) {
+    let totalCost = 0;
+    array.forEach(groceryObj => {
+        totalCost = totalCost + (groceryObj.price * groceryObj.quantity)
+    })
+
+    return parseFloat(totalCost.toFixed(1))
+}
+
+
+// 1 bottle of milk:
+getTotalPrice([
+    { product: "Milk", quantity: 1, price: 1.50 }
+  ]) 
+//   ➞ 1.5
+  
+  // 1 bottle of milk & 1 box of cereals:
+  getTotalPrice([
+    { product: "Milk", quantity: 1, price: 1.50 },
+    { product: "Cereals", quantity: 1, price: 2.50 }
+  ]) 
+//   ➞ 4
+  
+  // 3 bottles of milk:
+  getTotalPrice([
+    { product: "Milk", quantity: 3, price: 1.50 }
+  ]) 
+//   ➞ 4.5
+  
+  // Several groceries:
+  getTotalPrice([
+    { product: "Milk", quantity: 1, price: 1.50 },
+    { product: "Eggs", quantity: 12, price: 0.10 },
+    { product: "Bread", quantity: 2, price: 1.60 },
+    { product: "Cheese", quantity: 1, price: 4.50 }
+  ]) 
+//   ➞ 10.4
+  
+  // Some cheap candy:
+  getTotalPrice([
+    { product: "Chocolate", quantity: 1, price: 0.10 },
+    { product: "Lollipop", quantity: 1, price: 0.20 }
+  ]) 
+//   ➞ 0.3
+
