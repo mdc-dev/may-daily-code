f87c36d85e86ca06267c049b9bb04432e2291397
Matthew Courtney <mcourtdev@gmail.com>
2022-05-13 00:17:52 -0400

    problem solution for day4

--- may-daily-code.js
+++ may-daily-code.js
@@ -107,3 +107,45 @@ getTotalPrice([
   ]) 
 //   ➞ 0.3
 
+// May 4 Burglary Series (15): Number of Occurrences 
+// To further increase the statistical knowledge of your fight, count the number of times 
+// a certain adjective was used. Given an object that contains several adjectives as values, 
+// return a new object where you count the ocurrences of each adjective.
+
+function countNumberOfOccurrences(object) {
+    let occurrencesObj = {};
+    let array = Object.values(object)
+    array.forEach(insult => {
+        if(!Object.keys(occurrencesObj).includes(insult)) {
+            occurrencesObj[insult] = 1
+        } else {
+            occurrencesObj[insult] ++;
+        }
+    })
+
+    return occurrencesObj;
+}
+
+countNumberOfOccurrences({
+    a: "moron",
+    b: "scumbag",
+    c: "moron",
+    d: "idiot",
+    e: "idiot"
+  }) 
+//   ➞ { moron: 2, scumbag: 1, idiot: 2 }
+  
+  
+  countNumberOfOccurrences({
+    a: "moron",
+    b: "moron",
+    c:"moron"
+  }) 
+//   ➞ { moron: 3 }
+  
+  
+  countNumberOfOccurrences({
+    a: "idiot",
+    b: "scumbag"
+  }) 
+//   ➞ { idiot: 1, scumbag: 1 }
