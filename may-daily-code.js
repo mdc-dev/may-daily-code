959561fe56531b00134b056689700c3cae40ba1d
Matthew Courtney <mcourtdev@gmail.com>
2022-05-17 13:26:45 -0400

    problem solution for day 17 part 1

--- may-daily-code.js
+++ may-daily-code.js
@@ -537,7 +537,6 @@ function getFrequencies(array) {
     let responseObj = {}
     array.forEach(num => {
         Object.keys(responseObj).includes(num) || Object.keys(responseObj).includes(num.toString()) ? responseObj[num] ++ : responseObj[num] = 1
-        console.log(responseObj)
     }) 
 
     return responseObj
@@ -556,3 +555,62 @@ getFrequencies([true, false, true, false, false])
 // ➞ {}
 
 
+
+// May 17 Persistance
+// The additive persistence of an integer, n, is the number of times you have to replace n with the sum of 
+// its digits until n becomes a single digit integer. The multiplicative persistence of an integer, n, is the 
+// number of times you have to replace n with the product of its digits until n becomes a single digit integer.
+// Create two functions that take an integer as an argument and:
+// Return its additive persistence.
+// Return its multiplicative persistence.
+
+function additivePersistence(number) {
+    let count = 1
+    const firstNumber = number.toString().split('').reduce((a, b) => parseInt(a) + parseInt(b), 0);
+    if(firstNumber.toString().length === 1) {
+        return 0
+    } else {
+        function addDigits(num) {
+            count ++
+            return num.toString().split('').reduce((a, b) => parseInt(a) + parseInt(b), 0);
+        }   
+        let num = addDigits(firstNumber);
+        num.toString().split('').length > 1 ? addDigits(num) : num
+        return count
+    }
+}
+
+
+
+additivePersistence(1679583) 
+// ➞ 3
+// 1 + 6 + 7 + 9 + 5 + 8 + 3 = 39
+// 3 + 9 = 12
+// 1 + 2 = 3
+// It takes 3 iterations to reach a single-digit number.
+
+additivePersistence(123456) 
+// ➞ 2
+// 1 + 2 + 3 + 4 + 5 + 6 = 21
+// 2 + 1 = 3
+
+additivePersistence(6) 
+// ➞ 0
+// Because 6 is already a single-digit integer.
+
+// multiplicativePersistence(77) 
+// ➞ 4
+// 7 x 7 = 49
+// 4 x 9 = 36
+// 3 x 6 = 18
+// 1 x 8 = 8
+// It takes 4 iterations to reach a single-digit number.
+
+// multiplicativePersistence(123456) 
+// ➞ 2
+// 1 x 2 x 3 x 4 x 5 x 6 = 720
+// 7 x 2 x 0 = 0
+
+// multiplicativePersistence(4) 
+// ➞ 0
+// Because 4 is already a single-digit integer.
