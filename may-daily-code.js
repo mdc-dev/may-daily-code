9d00087456e0b0383152e4dc5767fe0b474de4b5
Matthew Courtney <mcourtdev@gmail.com>
2022-05-25 23:14:33 -0400

    no solution need to work on in morning

--- may-daily-code.js
+++ may-daily-code.js
@@ -823,3 +823,49 @@ removeLastVowel("Love is a serious mental disease.")
 
 removeLastVowel("Get busy living or get busy dying.")
 // ➞ "Gt bsy livng r gt bsy dyng"
+
+
+// May 24 Find all Digits
+// Taking each four digit number of an array in turn, return the number that you are on when 
+// all of the digits 0-9 have been discovered. If not all of the digits can be found, return 
+// "Missing digits!".
+
+function findAllDigits(array) {
+    const matchArr = ['0','1','2','3','4','5','6','7','8','9'];
+    let ourDigits = [];
+    let returnNum;
+
+    array.forEach(num => {
+        let numArr = num.toString().split('');
+        numArr.forEach(number => {
+            if(!ourDigits.includes(number)) {
+                ourDigits.push(number);
+            } 
+
+            console.log(number, ourDigits.sort((a,b) => a - b), matchArr)
+            if(ourDigits.includes(number) && ourDigits.sort((a,b) => a - b) === matchArr) {
+               console.log('Match')
+            }
+        })
+    });
+
+    if(!returnNum) {
+        console.log("Missing Numbers")
+    } else {
+        console.log(returnNum)
+    }
+}
+
+
+findAllDigits([5175, 4538, 2926, 5057, 6401, 4376, 2280, 6137, 8798, 9083]) 
+// ➞ 5057
+// digits found:  517-  4-38  29-6  -0
+
+// findAllDigits([5719, 7218, 3989, 8161, 2676, 3847, 6896, 3370, 2363, 1381]) 
+// ➞ 3370
+// digits found:  5719  -2-8  3---  --6-  ----  --4-  ----  ---0
+
+// findAllDigits([4883, 3876, 7769, 9846, 9546, 9634, 9696, 2832, 6822, 6868]) 
+// ➞ "Missing digits!"
+// digits found:  48-3  --76  ---9  ----  -5--  ----  ----  2---
+// 0 and 1 are missing
